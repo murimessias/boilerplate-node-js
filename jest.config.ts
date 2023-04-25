af361338bc43aa88e268dc0291624d90fa58c00a
Murilo Messias de Camargo <murilo.messias@gmail.com>
2023-04-25 11:12:25 -0300

    chore: add ts-jest

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import { compilerOptions } from "./tsconfig.json";
+import { pathsToModuleNameMapper, JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        isolatedModules: true,
+      },
+    ],
+  },
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>",
+  }),
+};
+
+export default jestConfig;
